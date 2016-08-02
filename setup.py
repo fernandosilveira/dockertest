24868202694c51b30b1c14aa6054383c7b79b684
Fernando Silveira <fernando.jorge.silveira@gmail.com>
2016-08-01 20:48:46 -0700

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils import core
+
+core.setup(
+    name='dockertools',
+    version='1.0',
+    description='Docker Tools',
+    author='Fernando Silveira',
+    author_email='fernando@f14a.com',
+    packages=['dockertools'],
+)
